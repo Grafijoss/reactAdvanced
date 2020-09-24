b557ecf93a902c284d5d5f1be2c2f4700a8511e3
grafijoss <grafijoss@hotmail.com>
2020-09-23 20:50:14 -0500

    vercel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDoom from 'react-dom'
 
-ReactDoom.render(<h1>Hola Patzi</h1>, document.getElementById('app'))
+ReactDoom.render(<h1>Prueba vercel</h1>, document.getElementById('app'))
